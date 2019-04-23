dbd1493660cb7cbc74c0e7e657597f7558a14004
Frank Wang <frank.wangtao@gmail.com>
2019-04-22 21:18:30 -0400

    Add command for move-to-let refactor

--- lsp-clojure.el
+++ lsp-clojure.el
@@ -5,6 +5,16 @@
 ;;; Code:
 (require 'lsp-mode)
 
+(defun lsp-clojure-move-to-let (binding-name)
+  "Move form at point into nearest existing let binding as BINDING-NAME."
+  (interactive "MBinding name: ") ;; Name of the let binding
+  (lsp--cur-workspace-check)
+  (lsp--send-execute-command
+   "move-to-let"
+   (list (concat "file://" buffer-file-name)
+         (- (line-number-at-pos) 1) ;; clojure-lsp expects line numbers to start at 0
+         (current-column)
+         binding-name)))
 
 (provide 'lsp-clojure)
 ;;; lsp-clojure.el ends here
