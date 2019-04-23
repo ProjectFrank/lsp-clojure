948747c5ecdd5632cfca8e60907a249e462d999f
Frank Wang <frank.wangtao@gmail.com>
2019-04-23 07:58:07 -0400

    Add command for add-missing-libspec refactoring

--- lsp-clojure.el
+++ lsp-clojure.el
@@ -47,5 +47,15 @@
          (- (line-number-at-pos) 1) ;; clojure-lsp expects line numbers to start at 0
          (current-column))))
 
+(defun lsp-clojure-add-missing-libspec ()
+  "Apply add-missing-libspec refactoring at point."
+  (interactive)
+  (lsp--cur-workspace-check)
+  (lsp--send-execute-command
+   "add-missing-libspec"
+   (list (concat "file://" buffer-file-name)
+         (- (line-number-at-pos) 1) ;; clojure-lsp expects line numbers to start at 0
+         (current-column))))
+
 (provide 'lsp-clojure)
 ;;; lsp-clojure.el ends here
